bd87a6e6f95e5221c889d0b0f87fbc67dd150da5
Harsha Jain <jaintpharsha@gmail.com>
2021-05-06 09:51:09 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -2,15 +2,3 @@ variable "AWS_REGION" {
 	type = string 
 	default = "ap-south-1"
 }
-
-variable "AWS_SECRET_KEY" {
-	type = string 
-	description = "Enter the Secret Key"
-	default = "h4/h7LIR827tzv4FaOlqLIH4b2iof3HDxwkX2l5J"
-}
-
-variable "AWS_ACCESS_KEY" {
-	type = string 
-	description = "Enter the Access Key"
-	default = "AKIAUQUNTD2JZSL5SRFU"
-}
