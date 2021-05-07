7fbcdd68ee178da4ff0dbc6724d4489ae8ab9a69
Harsha Jain <jaintpharsha@gmail.com>
2021-05-07 11:42:20 +0530

    update

--- backend/modules/aws/create_dynamodb/main.tf
+++ backend/modules/aws/create_dynamodb/main.tf
@@ -1,7 +1,9 @@
 resource "aws_dynamodb_table" "backend_dynamodb_lock_table" {
 	name = var.module_dynamodb_table_name
 	hash_key = var.module_dynamodb_hash_key
-	billing_mode = "PAY_PER_REQUEST"
+	billing_mode = "PROVISIONED"
+	read_capacity  = 5
+	write_capacity = 5
 	attribute {
 		name = var.module_dynamodb_hash_key
 		type = "S"
